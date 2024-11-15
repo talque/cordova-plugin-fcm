84b1a71646125cb1d874ee38db5ceed276c66990
Volker Braun <vbraun.name@gmail.com>
2024-11-15 09:35:02 -0500

    Update to 7.11.0, remove unused ionic script

--- scripts/install_ionic_dependencies.bat
+++
@@ -1,6 +0,0 @@
-cd node_modules
-cd cordova-plugin-fcm
-cd scripts
-node install_ionic_dependencies.js ionic
-node install_ionic_dependencies.js ionic/ngx
-node install_ionic_dependencies.js ionic/v4
